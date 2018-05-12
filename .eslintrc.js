03548eb83b2e4fe1cc7424655cf73694ad8cc02f
Sourav Ray <me@raysourav.com>
2018-05-12 11:29:29 +0530

    disable no-console rule for log and warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,8 +29,7 @@ module.exports = {
             { 
                 "allow": [
                     "log",
-                    "warn", 
-                    "error"
+                    "warn"
                 ] 
             }
         ]
